e4a49f4c0330f99c9c83527969eeb36dec89baec
Helene Bengtsson <helben2002@skola.goteborg.se>
2023-11-19 18:46:59 +0100

    finished getReply-method

--- Guesser.java
+++ Guesser.java
@@ -66,6 +66,11 @@ public class Guesser{
     // As long as it is not a valid reply (one of "T" and "F")
     // write an error message, and read a new reply.
     // When you have gotten a valid reply, return it.
+    reply = System.console().readLine();
+    while(!reply.equals("T") && !reply.equals("F")){
+      System.out.println("Please answer T or F");
+      reply = System.console().readLine();
+    }
     return reply;
   }
 
--- GuessingGame.java
+++ GuessingGame.java
@@ -1,6 +1,6 @@
 public class GuessingGame {
     public static void main(String[] args) {
-        Guesser newGame = new Guesser(0, 1000);
+        Guesser newGame = new Guesser(0, 10);
 
         newGame.start();
     }
